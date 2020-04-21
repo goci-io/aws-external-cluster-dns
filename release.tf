6bd3cc129b711c856472ac1fc9dab0c1bd101963
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-21 19:07:46 +0200

    expose role info, only set assume role if desired

--- release.tf
+++ release.tf
@@ -12,11 +12,11 @@ resource "helm_release" "external_dns" {
 
   values = [
     templatefile("${path.module}/defaults.yaml", {
-      hosted_zone_ids = data.aws_route53_zone.targets.*.zone_id
       domains         = var.domains
-      aws_region      = var.aws_region
-      iam_role_arn    = aws_iam_role.external_dns.arn
       update_policy   = var.update_records_policy
+      aws_region      = var.aws_region
+      iam_role_arn    = var.apply_assume_role_config ? aws_iam_role.external_dns.arn : ""
+      hosted_zone_ids = data.aws_route53_zone.targets.*.zone_id
       owner_id        = join("/", [var.cluster_fqdn, var.k8s_namespace])
     }),
     file("${var.helm_values_root}/values.yaml"),
